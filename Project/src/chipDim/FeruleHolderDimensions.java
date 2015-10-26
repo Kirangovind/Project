d6b93148a959d8af1d9a5e9228aaa2ace395c5b1
Kiran Govind <asdfjkl1234@live.co.uk>
2015-10-26 18:18:17 +0000

    chipDim: corrected depth problem and added diam allowance

--- Project/src/chipDim/FeruleHolderDimensions.java
+++ Project/src/chipDim/FeruleHolderDimensions.java
@@ -2,21 +2,22 @@ package chipDim;
 
 public class FeruleHolderDimensions {
 
-	private double d, w;
+	private double d, r;
 	 
 	// Asks the user to set the depth of the ferule holder and the
-	// width of the ferule.
-	public FeruleHolderDimensions (double e, double f) {
-		d = e;
-		w = f;
+	// width of the ferule and radiance allowance.
+	
+	public FeruleHolderDimensions (double depth, double diam, double diamAllow) {
+		d = depth;
+		r = (diam/2)+(diamAllow)/2;
 	}
 	
 	public double Angle() {
-		return (Math.toDegrees(Math.asin(w/d)));
+		return (Math.toDegrees(Math.asin(r/(d-(0.125/2)))));
 	}
 	
 	public double A() {
-		return (d*Math.tan(Math.toRadians(this.Angle())));
+		return (2*(d*Math.tan(Math.toRadians(this.Angle()))));
 	}
 	
 	public double C() {
--- Project/src/chipDim/FindFeruleHolderDimensions.java
+++ Project/src/chipDim/FindFeruleHolderDimensions.java
@@ -4,11 +4,11 @@ public class FindFeruleHolderDimensions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		FeruleHolderDimensions n_d = new FeruleHolderDimensions (0.5, 0.125);
+		// (Ferule Holder Depth, Diameter of Ferule, Diameter Allowance)
+		FeruleHolderDimensions n_d = new FeruleHolderDimensions (1.5, 1.25, 0.05);
 		System.out.println("Angle: "+n_d.Angle()+"degrees");
-		System.out.println("a: "+n_d.A()+"mm");
-		System.out.println("c: "+n_d.C()+"mm");
+		System.out.println("Base: "+(n_d.A())+"mm");
+		System.out.println("Side: "+n_d.C()+"mm");
 	}
 
 }
